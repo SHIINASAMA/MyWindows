0eb5441d59c69d567047dd9c9b75dc5d8389a7a2
kaoru <shiina_kaoru@outlook.com>
2024-08-25 13:56:34 +0800

    winget 初始化脚本

--- Install.ps1
+++ Install.ps1
@@ -1,8 +1,18 @@
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
 winget source update
 winget upgrade Microsoft.AppInstaller
 winget update --all
 
-winget install agalwood.Motrix `
+winget install `
+   agalwood.Motrix `
     ClashVergeRev.ClashVergeRev `
     CoreyButler.NVMforWindows `
     7zip.7zip `
@@ -32,4 +42,4 @@ winget install agalwood.Motrix `
     AntibodySoftware.WizTree
 
 git clone https://github.com/microsoft/vcpkg C:/vcpkg
-C:/vcpkg/bootstrap-vcpkg.bat
+& "C:/vcpkg/bootstrap-vcpkg.bat"
